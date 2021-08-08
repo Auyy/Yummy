e63ddc63fa549b608dc5bcfb4c7ad22ea2d3baf9
Auyy <71812937+Auyy@users.noreply.github.com>
2021-08-08 14:43:19 +0700

    sqlite

---
+++ MixYummy/MixYummy/Data/MixYummyContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using MixYummy.Models;
+
+namespace MixYummy.Data
+{
+    public class MixYummyContext : DbContext
+    {
+        public MixYummyContext(DbContextOptions<MixYummyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TblProduct> TblProduct { get; set; }
+    }
+}
---
+++ MixYummy/MixYummy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MixYummy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
